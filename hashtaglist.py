bf6f18ec11e89e620d647e25a7b31fd4a16f0522
diexel64 <48530929+diexel64@users.noreply.github.com>
2020-06-27 11:01:36 +0200

    Create hashtaglist.py
    Lists of hashtags for the robot to use.

---
+++ hashtaglist.py
@@ -0,0 +1,37 @@
+# STRINGS OF HASHTAGS BY QUANTITY OF PICS
+
+# Most popular pic hashtags related to travel
+travel0 = "#travel #travelgram #travelphotography #instatravel #traveling #travelling #travelblogger #traveler #traveller #igtravel #traveltheworld #travelingram #travelblog #mytravelgram #travels #instatraveling #traveladdict #travelphoto #travellife"
+# Around 5000K-10 000 K pic hashtags related to travel
+travel1 = "#travelbug #travelstoke #traveldeeper #travelholic #ilovetravel #travelpic #worldtraveler #travelers #travelblogger #igtravel #traveler"
+# Around 1000K-5000K pic hashtags related to travel
+travel2 = "#travelph #bbctravel #travelguide #lovetotravel #lovetravel #worldtravel #instatravelling #travelersnotebook #travellover #travelgoals #travelphotographer #travelandlife #iamatraveler #travelworld #travelasia #welltraveled #travelbag #travelbloggers #ig_travel #travelislife #adventuretravel #travelaroundtheworld #travelingtheworld #travelstories #travelstyle #globe_travel #worldtraveller #travelnow #traveltime"
+# Around 500K-1000K pic hashtags related to travel
+travel3 = "#travelingourplanet #bestintravel #forbestravelguide #mytravel #traveljournal #lovetraveling #travellingtheworld #travelalone #solotraveller #traveloften #traveldestination #phototravel #travelinggram #travelblogging #travelgrams #travelforlife #teamtravelers #livetotravel #travelbloggerlife #travellust"
+# Around 200K-500K pic hashtags related to travel
+travel4 = "#travelista #travelingpost #travelchannel #travelmood #travelwithus #travellerslife #travelstory #world_besttravel #travelmoments #travelrack #itravel #travelingphotographer #greatesttravels #travelpreneur #mytravels #travellog #travelpost #travelinsta #travelife #mytraveldiary #totravelistolive #iamtraveler #gotravel #travelphotograpy #travelpictures #letstravel #travelinfluencer #safetravels #traveldestinations #ilovetraveling #traveladventure #lifeintravel #weliketotravel #soultravel #travelgo #travelaround #travelforever #travelinstagram #worldtravelers #longtravel #travelwell"
+# Around 100K-200K pic hashtags related to travel
+travel5 = "#travelersnote #wetravel #traveligram #travelerslife #ig_travelerworld #travelwithstyle #inspiredtravels #huffposttravel #travelingmodel #travelbreak #theroadlesstraveled #the_daily_traveller #travelblogs #travelboy #visualtraveller #lovetravels #besttravel #betraveler #soultravelers #traveltrip #amazingtravelbeauty #globaltravel #travellingourplanet #travelsize #travelersjournal #travelfreak #globetravel #happytravels #travelmode #travelingaroundtheworld #travelingalone #travelbloggeres #travellist #travelbloger #travel_photography #instratravel #travelmoreworryless #everytraveler #traveler_stories #weworktotravel #travelinglife #fulltimetravel #addictedtotravel #welovetravel #travelust #intravelist #travellinglife #wearetravelers #holidaytravel #traveltherenext #whereitravel #globaltravelgram #travelgasm #traveldreams #travelandexplore #travelgood #passionfortravel #travellingalone #inspiredtraveller #travelover #blogtravel #travelfar #roadlesstravelled #travelphotografy #instatravellers #happytraveller"
+# Around 75K-100K pic hashtags related to travel
+travel6 = "#serialtraveler #mybudgettravel #travel_diaries #visiontravel #traveled #mybestintravel #travelandliving #dairytravel #welovetotravel #dreamingtravel #keeptravelling #natgeotraveler"
+# Around 50K-75K pic hashtags related to travel
+travel7 = "#travelingstuff #travelexperiences #travelapp #travelslifee #travel_awesome #endlesstraveling #globetravelgram #travelfly #letsgotravel #letstraveltoday #traveldays #travellersofinstagram #travelwellshootoften #roamtravels #instantravel #frequenttraveller #backpacktravel #travellingaround #travelearth #travellingstuff #travelhunting #travelenjoyrespect #goodtravel #travelfreedom #travelcolorfully #travellingdestinations"
+# Around 30K-50K pic hashtags related to travel
+travel8 = "#officialtravellers #traveltv #ig_shotz_travel #travelingtheworldd #mytravelingram #natgeotravels #havepassportwilltravel #traveltheworldofficial #travelmap #travelersrest #internationaltraveler #worldtravelphotography #globaltraveller #travelpostdaily #lifeoftravel #beyondtravels #traveltimes #travelspot #global_travel #lifeofatraveler #theroadlesstravelled #instatravelers #travelmassive #thinklesstravelmore #fortheloveoftravel #alwaystravelling #iwanttotravel #travelicious #iliketravel #travellingdiaries #travelhere #thetravellersway #traveltolearn #uncommontravel #moretravel #travelshoes #authentictravel #travelcase #liketravel #travelalot #travelshoot #beautifultravel #travelist #pathlesstraveled #bestcommunitytravel #globaltravelers #nexttravel #travelintheworld #fototravel #mytravelblog #travelergram #travelsandchill #love_travel"
+# Around 30K pic hashtags related to travel
+travel9 = "#menwhotravel #outdoorstravel #travelfinds #letstravelmore #travelview #travelfree #enjoytravelling #somewhere_travel #travelistolive #solotravellers #4travel #travelourplanet #throwbacktravel #thetravellerslist #nonstoptravel #weekendtravels #travelinfrance #traveltraveltravel #needtotravel"
+
+alllists = travel0 + " " + travel1 + " " + travel2 + " " + travel3 + " " + travel4 + " " + travel5 + " " + travel6 + " " + travel7 + " " + travel8 + " " + travel9
+
+HASHTAGS = {
+    'TRAVEL0': travel0,
+    'TRAVEL1': travel1,
+    'TRAVEL2': travel2,
+    'TRAVEL3': travel3,
+    'TRAVEL4': travel4,
+    'TRAVEL5': travel5,
+    'TRAVEL6': travel6,
+    'TRAVEL7': travel7,
+    'TRAVEL8': travel8,
+    'ALL': alllists
+}
